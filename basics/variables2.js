211271156a029cf31b3dbca4b971935f5fee7438
Raj Poddar <77565742+rajakrp18@users.noreply.github.com>
2024-10-04 17:10:40 +0000

    type conversion

---
+++ basics/variables2.js
@@ -0,0 +1,50 @@
+// Type Conversions
+
+let score = 33
+console.log(typeof score); //date-type is number in both 
+console.log(typeof (score));
+
+let score1 = "33"
+console.log(typeof score1); //date-type is number in string 
+console.log(typeof (score1));
+
+let score2 = "33"
+let valueInNumber = Number(score2) //used to convert any datatype to number
+console.log(valueInNumber)
+console.log(typeof (valueInNumber))
+
+
+let score3 = "33abc"
+let valueInNumber1 = Number(score3) //used to convert any datatype to number
+console.log(valueInNumber1) //value is NaN
+console.log(typeof (valueInNumber1)) //but conversion is successfull
+
+let score4 = "null" //null as a string
+let valueInNumber2 = Number(score4) //used to convert any datatype to number
+console.log(valueInNumber2) //value is null is also coming NaN
+console.log(typeof (valueInNumber2)) //but conversion is successfull
+
+let score5 = null
+let valueInNumber3 = Number(score5) //used to convert any datatype to number
+console.log(valueInNumber3) //value is null is 0
+console.log(typeof (valueInNumber3)) //but conversion is successfull
+
+let score6 = undefined
+let valueInNumber4 = Number(score6) //used to convert any datatype to number
+console.log(valueInNumber4) //value is undefined is NaN
+console.log(typeof (valueInNumber4)) //but conversion is successfull
+
+let score7 = true
+let valueInNumber5 = Number(score7) //used to convert any datatype to number
+console.log(valueInNumber5) //value is true is 1
+console.log(typeof (valueInNumber5)) //but conversion is successfull
+
+let score8 = false
+let valueInNumber6 = Number(score8) //used to convert any datatype to number
+console.log(valueInNumber6) //value is false is 0
+console.log(typeof (valueInNumber6)) //but conversion is successfull
+
+// Notes
+// 33 ==> 33
+// "33" ==> NaN(Not a Number)
+// true=> 1, false=> 0
