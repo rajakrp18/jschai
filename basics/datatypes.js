1c1a3a8ec3fdf6581da2c8ff9c780613e922105f
Raj Poddar <77565742+rajakrp18@users.noreply.github.com>
2024-10-04 18:41:00 +0530

    Create datatypes.js

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) //we are using nodejs not browser
+
+// datatypes in js 
+let name ="Raj"   //string  
+let age =24       //number  
+let isLoggedIn=false //boolean
+
+
+//number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+console.table([name, age, isLoggedIn])
+console.log("typeof undefined is -->", typeof undefined);
+console.log("typeof null is -->", typeof null); //drawback of null which is considered as object
