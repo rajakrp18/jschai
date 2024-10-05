47110313de89d3eb79c90b1909a9e4d0dd05b633
Raj Poddar <77565742+rajakrp18@users.noreply.github.com>
2024-10-05 09:17:46 +0000

    operations in desc

---
+++ basics/operations.js
@@ -0,0 +1,29 @@
+// Operations
+
+// let value = 3
+// let negValue = -value 
+// console.log(negValue);
+
+// basics operations 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/2);
+// console.log(2%4);
+
+
+let str1="hello "
+let str2="Raj"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+
+console.log(3 + 4 * 5 % 3);
